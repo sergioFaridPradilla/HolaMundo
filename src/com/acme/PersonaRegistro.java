cd4dbfc21c8e106a71f5cd399a88951964ca60d8
Sergio <sergio_yamus@hotmail.com>
2023-10-08 08:24:06 -0700

    Se agregó una línea de código inservible

--- src/com/acme/PersonaRegistro.java
+++ src/com/acme/PersonaRegistro.java
@@ -4,6 +4,7 @@ public record PersonaRegistro(String nombre, String apellido) {
 
     @Override
     public String toString() {
+        System.out.println("Esta es una linea inservible");
         return "%s %s".formatted(nombre, apellido);
     }
 }
