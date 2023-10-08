37b716f27c57a6ee7de87a925be23c39f1f2e41a
Sergio <sergio_yamus@hotmail.com>
2023-10-08 07:32:44 -0700

    Se creo la clase persona

--- src/com/acme/HolaMundo.java
+++ src/com/acme/HolaMundo.java
@@ -3,7 +3,9 @@ package com.acme;
 public class HolaMundo {
     public static void main(String[] args) {
         Mensaje msg = new Mensaje();
-        String nombre = msg.solicitaDatos("Excdribe tu nombre: ");
-        msg.saludar(nombre);
+        String nombre = msg.solicitaDatos("Excribe tu nombre: ");
+        String apellido = msg.solicitaDatos("Excribe tu apellido: ");
+        Persona p1 = new Persona(nombre, apellido);
+        msg.saludar(p1);
     }
 }
--- src/com/acme/Mensaje.java
+++ src/com/acme/Mensaje.java
@@ -5,11 +5,11 @@ import java.util.Scanner;
 public class Mensaje {
 
     public String solicitaDatos( String mensaje){
-        System.out.println(mensaje);
+        System.out.print(mensaje);
         Scanner scn = new Scanner(System.in);
         return scn.nextLine();
     }
-    public void saludar(String nombre) {
-        System.out.printf("Hola %s, bienvenido a java ", nombre);
+    public void saludar(Persona p) {
+        System.out.printf("Hola %s, bienvenido a java ", p);
     }
 }
