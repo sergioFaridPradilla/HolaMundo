330646a4487630f17d0342549f9d4588344950ce
sergioFaridPradilla <99918531+sergioFaridPradilla@users.noreply.github.com>
2023-10-23 19:37:42 -0700

    Comentario sobre codigo inservible

--- src/com/acme/PersonaRegistro.java
+++ src/com/acme/PersonaRegistro.java
@@ -4,7 +4,8 @@ public record PersonaRegistro(String nombre, String apellido) {
 
     @Override
     public String toString() {
-        System.out.println("Esta es una linea inservible");
+        //Sergio comente la linea de codigo que considero se puede eliminar
+        //System.out.println("Esta es una linea inservible");
         return "%s %s".formatted(nombre, apellido);
     }
 }
